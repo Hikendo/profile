715e473c45fd803a584d67528f94ff9dc1131217
José Manuel Martínez Antonio <josema.k303@gmail.com>
2024-06-04 23:54:00 -0600

    Modificado blur, modificado fondo svg

--- src/app/profile/components/menu-profile/menu-profile.component.ts
+++ src/app/profile/components/menu-profile/menu-profile.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleLeft, faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from '../../../services/user.service';
 @Component({
   selector: 'app-menu-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './menu-profile.component.html',
   styleUrl: './menu-profile.component.css'
 })
 export class MenuProfileComponent {
+  public menuFloat= inject(UserService)
+  faMenu= faBars;
 
+  toggleShowMenu( item : string):void{
+   // alert(item);
+
+     this.menuFloat.toggleShowMenu(item);
+
+
+
+  }
 }
--- src/app/profile/layout/layout-page/layout-page.component.ts
+++ src/app/profile/layout/layout-page/layout-page.component.ts
@@ -6,8 +6,8 @@ import JobExperienceComponent from '../../pages/job-experience/job-experience.co
 import SkillsComponent from '../../pages/skills/skills.component';
 import AcademicBackgroundComponent from '../../pages/academic-background/academic-background.component';
 import { UserService } from '../../../services/user.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faAngleLeft, faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import { MenuProfileComponent } from '../../components/menu-profile/menu-profile.component';
+
 
 
 
@@ -15,20 +15,10 @@ import { faAngleLeft, faBars, faClose } from '@fortawesome/free-solid-svg-icons'
   selector: 'app-layout-page',
   standalone: true,
   imports: [RouterLink, RouterLinkActive, RouterModule, UserComponent, SummaryComponent,
-     JobExperienceComponent, SkillsComponent, AcademicBackgroundComponent, UserComponent, FontAwesomeModule],
+     JobExperienceComponent, SkillsComponent, AcademicBackgroundComponent, UserComponent , MenuProfileComponent],
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.css'
 })
 export default class LayoutPageComponent {
-  public menuFloat= inject(UserService)
-  faMenu= faBars;
 
-  toggleShowMenu( item : string):void{
-   // alert(item);
-
-     this.menuFloat.toggleShowMenu(item);
-
-
-
-  }
 }
